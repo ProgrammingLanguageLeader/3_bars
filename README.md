1ec8538f93ee03367bc09993422e628954d93843
ProgrammingLanguageLeader <vip.shoroch@gmail.com>
2017-10-13 04:11:52 +0300

    Implemented bars.py and README.md

--- README.md
+++ README.md
@@ -1,22 +1,32 @@
-# Ближайшие бары
+# The Closest Bars
 
-[FIXME. Здесь будет описание проекта]
+A program which finds the biggest, the smallest and the nearest bars in Moscow.
 
-# Как запустить
+# How to use
 
-Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+This script requires Python 3.5 (or higher) installed on your machine
 
-Запуск на Linux:
+A launching on Linux:
 
 ```#!bash
-
 $ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
-
+Enter a longitude and a latitude: 37.657179 55.758637
+The biggest bar
+Name: Спорт бар «Красная машина»
+Address: Автозаводская улица, дом 23, строение 1
+Phone number: (905) 795-15-84
+The smallest bar
+Name: Сушистор
+Address: Михалковская улица, дом 8
+Phone number: (495) 230-00-00
+The closest bar
+Name: Каро фильм
+Address: улица Земляной Вал, дом 33
+Phone number: (495) 970-17-80
 ```
 
-Запуск на Windows происходит аналогично.
+The launch on Windows is similar.
 
-# Цели проекта
+# Project Goals
 
-Код создан в учебных целях. В рамках учебного курса по веб-разработке - [DEVMAN.org](https://devman.org)
+The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- bars.py
+++ bars.py
@@ -1,21 +1,146 @@
 import json
+import os
+import urllib.request
+from math import asin
+from math import sin
+from math import cos
+from math import sqrt
+from math import radians
+
+
+def get_bars_features(bar_data):
+    return bar_data['features']
+
+
+def get_bar_attributes(feature):
+    return feature['properties']['Attributes']
+
+
+def get_bar_seats_count(feature):
+    return get_bar_attributes(feature)['SeatsCount']
+
+
+def get_bar_name(feature):
+    return get_bar_attributes(feature)['Name']
+
+
+def get_bar_address(feature):
+    return get_bar_attributes(feature)['Address']
+
+
+def get_bar_phone(feature):
+    return get_bar_attributes(feature)['PublicPhone'][0]['PublicPhone']
+
+
+def get_bar_main_info(features):
+    bar_info = \
+        'Name: {}\n' \
+        'Address: {}\n' \
+        'Phone number: {}'.format(
+            get_bar_name(features),
+            get_bar_address(features),
+            get_bar_phone(features)
+        )
+    return bar_info
+
+
+def get_bar_geometry(feature):
+    return feature['geometry']['coordinates']
+
+
+def calculate_distance(longitude1, latitude1, longitude2, latitude2):
+    lon1, lat1, lon2, lat2 = map(
+        radians, [longitude1, latitude1, longitude2, latitude2]
+    )
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    return 2 * asin(
+        sqrt(
+            sin(dlat / 2) ** 2 +
+            cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+        )
+    ) * 6367
+
+
+def get_distance_to_bar(bar_data, longitude, latitude):
+    bar_longitude, bar_latitude = get_bar_geometry(bar_data)
+    return calculate_distance(
+        bar_longitude, bar_latitude,
+        longitude, latitude
+    )
+
+
+def fetch_data_from_web(api_key):
+    response = urllib.request.urlopen(
+        'https://apidata.mos.ru/v1/features/1796'
+        '?api_key={}'.format(api_key)
+    )
+    json_data = response.read()
+    return json_data
 
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except IOError:
+        return None
 
 
 def get_biggest_bar(data):
-    pass
+    return max(
+        get_bars_features(data),
+        key=lambda bar_data: get_bar_seats_count(bar_data)
+    )
 
 
 def get_smallest_bar(data):
-    pass
+    return min(
+        get_bars_features(data),
+        key=lambda bar_data: get_bar_seats_count(bar_data)
+    )
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    return min(
+        get_bars_features(data),
+        key=lambda bar_data: get_distance_to_bar(
+            bar_data, longitude, latitude
+        )
+    )
 
 
 if __name__ == '__main__':
-    pass
+    bars_info = load_data('data.json')
+    if not bars_info:
+        api_key = os.environ.get('MOS_RU_API_KEY')
+        if not api_key:
+            exit(
+                'Please, set MOS_RU_API_KEY variable to fetch data about '
+                'bars from web or download it from mos.ru, name received '
+                'file as "data.json" and move it to the script directory'
+            )
+        bars_data = fetch_data_from_web(api_key)
+        if not bars_data:
+            exit('Connection problems')
+        with open('data.json', 'wb') as file:
+            file.write(bars_data)
+        bars_info = json.loads(bars_data, encoding='utf-8')
+
+    try:
+        longitude, latitude = [
+            float(number) for number in input(
+                'Enter a longitude and a latitude: '
+            ).split()
+        ]
+        biggest = get_biggest_bar(bars_info)
+        smallest = get_smallest_bar(bars_info)
+        closest = get_closest_bar(bars_info, longitude, latitude)
+        print('The biggest bar')
+        print(get_bar_main_info(biggest))
+        print('The smallest bar')
+        print(get_bar_main_info(smallest))
+        print('The closest bar')
+        print(get_bar_main_info(closest))
+    except ValueError:
+        exit('Check input data')
